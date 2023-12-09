6b78a19b71f559091568e91b5d4a916c0c9282c9
devkokov <dm.kokov@gmail.com>
2023-12-09 23:05:30 +0000

    Add answer for Day9 Part2

--- src/day9/Part2.kt
+++ src/day9/Part2.kt
@@ -5,7 +5,7 @@ import solution.Dataset
 class Part2: Part1() {
     override val datasets = setOf(
         Dataset("src/day9/data/test_input_part1.txt", "2"),
-        Dataset("src/day9/data/input.txt")
+        Dataset("src/day9/data/input.txt", "905")
     )
 
     override val forward = false
