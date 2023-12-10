ef74477bfdb6a94a4db388d808196a8cd65cd99a
devkokov <dm.kokov@gmail.com>
2023-12-10 18:38:48 +0000

    Simplify and optimise Day10 Part2

--- src/day10/Part2.kt
+++ src/day10/Part2.kt
@@ -20,26 +20,41 @@ class Part2 : Part1() {
         var area = 0
         val directionFactor = if (loopDirection > 0) 1 else -1
 
+        val pipesByX = mutableMapOf<Int, MutableList<Pair<Pair<Int, Int>, String>>>()
+        val pipesByY = mutableMapOf<Int, MutableList<Pair<Pair<Int, Int>, String>>>()
+
+        for (pipe in this.pipeCoords) {
+            if (!pipesByX.containsKey(pipe.value.first.first)) {
+                pipesByX[pipe.value.first.first] = mutableListOf()
+            }
+            if (!pipesByY.containsKey(pipe.value.first.second)) {
+                pipesByY[pipe.value.first.second] = mutableListOf()
+            }
+            pipesByX[pipe.value.first.first]!!.add(pipe.value)
+            pipesByY[pipe.value.first.second]!!.add(pipe.value)
+        }
+
         for (x in 0 until this.grid.count()) {
             for (y in 0 until this.grid[0].count()) {
-                if (this.pipeCoords.containsKey("$x:$y")){
+                if (this.pipeCoords.containsKey("$x:$y") || !pipesByX.containsKey(x) || !pipesByY.containsKey(y)) {
                     continue
                 }
 
-                val crossPipes = this.pipeCoords.filterValues { it.first.first == x || it.first.second == y }
-                val minX = crossPipes.filterValues { it.first.second == y && it.first.first < x }.maxWithOrNull { a, b -> a.value.first.first.compareTo(b.value.first.first) }
-                val maxX = crossPipes.filterValues { it.first.second == y && it.first.first > x }.minWithOrNull { a, b -> a.value.first.first.compareTo(b.value.first.first) }
-                val minY = crossPipes.filterValues { it.first.first == x && it.first.second < y }.maxWithOrNull { a, b -> a.value.first.second.compareTo(b.value.first.second) }
-                val maxY = crossPipes.filterValues { it.first.first == x && it.first.second > y }.minWithOrNull { a, b -> a.value.first.second.compareTo(b.value.first.second) }
+                val down = pipesByY[y]!!.filter { it.first.first < x }
+                val up = pipesByY[y]!!.filter { it.first.first > x }
+                val left = pipesByX[x]!!.filter { it.first.second < y }
+                val right = pipesByX[x]!!.filter { it.first.second > y }
 
-                if (minX == null || maxX == null || minY == null || maxY == null) {
+                if (down.isEmpty() || up.isEmpty() || left.isEmpty() || right.isEmpty()) {
                     continue
                 }
 
-                val checkFactor = when (char(maxY.value.first)) {
-                    "|" -> if (maxY.value.second == "up") -1 else 1
-                    "L" -> if (maxY.value.second == "left") -1 else 1
-                    "F" -> if (maxY.value.second == "up") -1 else 1
+                val rightBound = right.minWith { a, b -> a.first.second.compareTo(b.first.second) }
+
+                val checkFactor = when (char(rightBound.first)) {
+                    "|" -> if (rightBound.second == "up") -1 else 1
+                    "L" -> if (rightBound.second == "left") -1 else 1
+                    "F" -> if (rightBound.second == "up") -1 else 1
                     else -> 0
                 }
 
